c52ace2026aa3f8fdfb954f1b06eb3cf34c5ca59
Daniel Radu <danyhk94@gmail.com>
2020-01-25 12:23:20 +0200

    Fix wrong spritesheets path

--- Components/source/MainCharacter.cpp
+++ Components/source/MainCharacter.cpp
@@ -25,27 +25,27 @@ void SuperBullet::MainCharacter::SetPosition(const Vector2f & position)
 void SuperBullet::MainCharacter::LoadRifleSpriteSheets()
 {
 	std::shared_ptr<Texture> idle_ss = std::make_shared<Texture>();
-	idle_ss->loadFromFile("Assets/Character/_spritesheets/rifle_idle.png");
+	idle_ss->loadFromFile("Assets/Character/Spritesheets/rifle_idle.png");
 	spritesheets.push_back(idle_ss);
 	mSpriteComponent->AddAnimation("idle", *idle_ss, SuperBullet::Vector2u(0, 0), 3, 7, 20);
 
 	std::shared_ptr<Texture> melee_ss = std::make_shared<Texture>();
-	melee_ss->loadFromFile("Assets/Character/_spritesheets/rifle_melee.png");
+	melee_ss->loadFromFile("Assets/Character/Spritesheets/rifle_melee.png");
 	spritesheets.push_back(melee_ss);
 	mSpriteComponent->AddAnimation("melee", *melee_ss, SuperBullet::Vector2u(0, 0), 4, 3, 12);
 
 	std::shared_ptr<Texture> move_ss = std::make_shared<Texture>();
-	move_ss->loadFromFile("Assets/Character/_spritesheets/rifle_move.png");
+	move_ss->loadFromFile("Assets/Character/Spritesheets/rifle_move.png");
 	spritesheets.push_back(move_ss);
 	mSpriteComponent->AddAnimation("move", *move_ss, SuperBullet::Vector2u(0, 0), 3, 7, 20);
 
 	std::shared_ptr<Texture> reload_ss = std::make_shared<Texture>();
-	reload_ss->loadFromFile("Assets/Character/_spritesheets/rifle_reload.png");
+	reload_ss->loadFromFile("Assets/Character/Spritesheets/rifle_reload.png");
 	spritesheets.push_back(reload_ss);
 	mSpriteComponent->AddAnimation("reload", *reload_ss, SuperBullet::Vector2u(0, 0), 3, 7, 20);
 
 	std::shared_ptr<Texture> shoot_ss = std::make_shared<Texture>();
-	shoot_ss->loadFromFile("Assets/Character/_spritesheets/rifle_shoot.png");
+	shoot_ss->loadFromFile("Assets/Character/Spritesheets/rifle_shoot.png");
 	spritesheets.push_back(shoot_ss);
 	mSpriteComponent->AddAnimation("shoot", *shoot_ss, SuperBullet::Vector2u(0, 0), 3, 1, 3);
 
